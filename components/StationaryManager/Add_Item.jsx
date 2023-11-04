838ec427d5899d6dbb2903c275bbfaf934075e95
Mudra Bhalodiya <mudrabhalodiya@gmail.com>
2023-11-04 14:48:40 +0530

    C:/Program Files/Git/Request/addItem page

--- components/StationaryManager/Add_Item.jsx
+++ components/StationaryManager/Add_Item.jsx
@@ -2,54 +2,11 @@ import React, { useState } from "react";
 import "./Add_Item.css";
 
 export default function Add_Item() {
-	const [catg1, setCatg1] = useState(true);
-	const [catg2, setCatg2] = useState(false);
-	const [catg3, setCatg3] = useState(false);
-	const [catg4, setCatg4] = useState(false);
-	const [catg5, setCatg5] = useState(false);
-
-	const ShowCatg1 = () => {
-		setCatg1(true);
-		setCatg2(false);
-		setCatg3(false);
-		setCatg4(false);
-		setCatg5(false);
-	};
-
-	const ShowCatg2 = () => {
-		setCatg1(false);
-		setCatg2(true);
-		setCatg3(false);
-		setCatg4(false);
-		setCatg5(false);
-	};
-
-	const ShowCatg3 = () => {
-		setCatg1(false);
-		setCatg2(false);
-		setCatg3(true);
-		setCatg4(false);
-		setCatg5(false);
-	};
-
-	const ShowCatg4 = () => {
-		setCatg1(false);
-		setCatg2(false);
-		setCatg3(false);
-		setCatg4(true);
-		setCatg5(false);
-	};
-
-	const ShowCatg5 = () => {
-		setCatg1(false);
-		setCatg2(false);
-		setCatg3(false);
-		setCatg4(false);
-		setCatg5(true);
-	};
-
 	return (
 		<div className="main_Add_item">
+			<div className="d_h1_ad_item">
+				<h1 className="h1_ad_item">Stationary Manager</h1>
+			</div>
 			<div className="main_form1">
 				<div className="head">Add New Items</div>
 				<br />
@@ -64,35 +21,35 @@ export default function Add_Item() {
 						<option
 							value="Project Materials"
 							className="slct_ctg_opt"
-							onClick={ShowCatg1}
+							// onClick={ShowCatg1}
 						>
 							Project Materials
 						</option>
 						<option
 							value="Writting Materials"
 							className="slct_ctg_opt"
-							onClick={ShowCatg2}
+							// onClick={ShowCatg2}
 						>
 							Writting Materials
 						</option>
 						<option
 							value="Study Materials"
 							className="slct_ctg_opt"
-							onClick={ShowCatg3}
+							// onClick={ShowCatg3}
 						>
 							Study Materials
 						</option>
 						<option
 							value="Electrical Items"
 							className="slct_ctg_opt"
-							onClick={ShowCatg4}
+							// onClick={ShowCatg4}
 						>
 							Electrical Items
 						</option>
 						<option
 							value="Miscellaneous"
 							className="slct_ctg_opt"
-							onClick={ShowCatg5}
+							// onClick={ShowCatg5}
 						>
 							Miscellaneous
 						</option>
