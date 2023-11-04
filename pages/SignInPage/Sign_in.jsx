ab46b74c091b96ff6decab6cb6be72002d1e2e9f
parthtpujara@gmail.com <ParthtPujara@gmail.com>
2023-11-04 10:59:04 +0530

    Status added auth js

--- pages/SignInPage/Sign_in.jsx
+++ pages/SignInPage/Sign_in.jsx
@@ -3,49 +3,66 @@ import "./Sign_in.css";
 import logo from "../../public/Images/signIn_logo.png";
 
 export default function Sign_in() {
-	return (
-		<div>
-			<div className="Main_Div_Signin">
-				<div className="Main_div2_Signin">
-					<div className="Div1_Signin">
-						<h1 className="SignIn_h1">Sign In</h1>
-						<p className="p1_Div1_Signin">Provide your Email and Password</p>
+  const signIn = async () => {
+    const email = document.getElementById("email_Inpt").value;
+    const password = document.getElementById("pass_Inpt").value;
+    const header = {
+      "Content-type": "application/json",
+    };
+    const body = {
+      email: email,
+      password: password,
+    };
 
-						<div className="role_dd_div">
-							<label htmlForfor="role_dd" className="role_dd_lbl">
-								Select Role :
-							</label>
+    const response = await fetch("http://192.168.248.100:2222/login", {
+      method: "GET",
+      headers: header,
+      body: JSON.stringify(body),
+    });
 
-							<select id="role_dd" className="role_dd_select_signin">
-								<option value="Branch Manager">Branch Manager</option>
-								<option value="Branch Worker">Branch Worker</option>
-								<option value="Stationary Manager">Stationary Manager</option>
-							</select>
-						</div>
+    const data = await response.json();
+    console.log(data);
 
-						<div className="email_inpt_div">
-							<label htmlFor="email_Inpt" className="email_inpt_lbl">
-								Email Id :
-							</label>
-							<input id="email_Inpt" />
-						</div>
+    if (data.status) {
+      alert("Login Successful");
+    } else {
+      alert("Login Failed");
+    }
 
-						<div className="pass_inpt_div">
-							<label htmlFor="pass_Inpt" className="pass_inpt_lbl">
-								Password :
-							</label>
-							<input id="pass_Inpt" />
-						</div>
+    return data;
+  };
 
-						<div className="signin_btn_div">
-							<button className="signin_btn">Sign In</button>
-						</div>
-					</div>
-					<div className="Div2_Signin">
-						<img className="logo_img_signin" src={logo} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="Main_Div_Signin">
+        <div className="Main_div2_Signin">
+          <div className="Div1_Signin">
+            <h1 className="SignIn_h1">Sign In</h1>
+            <p className="p1_Div1_Signin">Provide your Email and Password</p>
+
+            <div className="email_inpt_div">
+              <label htmlFor="email_Inpt" className="email_inpt_lbl">
+                Email Id :
+              </label>
+              <input id="email_Inpt" />
+            </div>
+
+            <div className="pass_inpt_div">
+              <label htmlFor="pass_Inpt" className="pass_inpt_lbl">
+                Password :
+              </label>
+              <input id="pass_Inpt" />
+            </div>
+
+            <div className="signin_btn_div">
+              <button className="signin_btn">Sign In</button>
+            </div>
+          </div>
+          <div className="Div2_Signin">
+            <img className="logo_img_signin" src={logo} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
